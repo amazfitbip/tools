ab67bab7b842712dc0f884ed88a6016c51f98158
dpeddi <eddi@dpeddi.com>
2017-08-26 13:32:38 +0200

    adjust some translation to fit the chinese character width and improve patcher

--- zh-cn2en.py
+++ zh-cn2en.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os,sys
+import os,sys,argparse
 reload(sys)  
 sys.setdefaultencoding('utf8')
 
@@ -13,27 +13,92 @@ translator = Translator()
 #.decode("hex")
 
 
+parser = argparse.ArgumentParser(description='Auto-translate and patch firmware files')
+parser.add_argument('-l', '--language', dest='language', help='en|it|de ...', required=True)
+parser.add_argument('-o', '--output')
+parser.add_argument('-v', dest='verbose', action='store_true', help='patch firmware file')
+parser.add_argument('-a', dest='analyze', action='store_true', help='analyze input file')
+#parser.set_defaults(language='en', baz='badger')
+args = parser.parse_args()
+
+print args.language
+#sys.exit(12)
+
 ##TODO
 ## the pattern must be ordered from the longer to the shorter 
 ## each pattern must be searched into the binary + 00 and replaced with the new string + 00 
 
+#https://wiki.python.org/moin/HowTo/Sorting
+#>>> student_tuples = [
+#        ('john', 'A', 15),
+#        ('jane', 'B', 12),
+#        ('dave', 'B', 10),
+#]
+#>>> sorted(student_tuples, key=lambda student: student[2])   # sort by age
+#[('dave', 'B', 10), ('jane', 'B', 12), ('john', 'A', 15)]
+
+
 fileName = "zh-cn2en.txt"
+
+translation_tuples = []
 
 out = open("zh-cn2en.out", mode='w')
 with open(fileName, mode='r') as file:
     for l in file:
-	line=l.rstrip('\n')
-	string_hex=line.split("|")[0]
+	line=l.lstrip('#').rstrip('\n')
+	string_hex=" ".join(c for c in line.split("|")[0].split())
 	string_cn="".join(c for c in string_hex.split()).decode("hex")
 	if len(line.split("|")) == 2:
 	    string_en=line.split("|")[1]
-	    out.write(line+"\n")
 	    #check here if translation is longer then the original hex
 	else:
 	    string_en=translator.translate(string_cn, dest='en').text
-	    out.write(line+"|"+string_en+"\n")
 
-	print "%s => %s = %s" % (string_hex,string_cn,string_en)
+	# update translation file
+	if len(string_en) > len(string_cn):
+	    string_hex = "#"+string_hex
+
+	translation_tuple = tuple( [string_hex, string_en])
+	if len(string_en) <= len(string_cn) and translation_tuple not in translation_tuples:
+	    translation_tuples.append(translation_tuple)
+
+	out.write("%s|%s\n" % (string_hex, string_en))
+	print "%s => %s(%d) = %s(%d)" % (string_hex,string_cn,len(string_cn),string_en,len(string_en))
+
+translation_tuples_s = sorted(translation_tuples, key=lambda string_cn: len(string_cn[0]), reverse=True)
+
+if args.verbose:
+    for index in range(len(translation_tuples_s)):
+	print translation_tuples_s[index]
+
+	from bitstring import ConstBitStream
+	from bitstring import BitArray
+	# Can initialise from files, bytes, etc.
+	#s = ConstBitStream(filename='Mili_chaohu.fw')
+	# Search to Start of Frame 0 code on byte boundary
+	#found = s.find('0xffc0', bytealigned=True)
+
+	with open("Mili_chaohu.fw", "rb") as input_file:
+	    s = input_file.read()
+
+	find_str = "".join(c for c in translation_tuples_s[index][0].split())
+	print find_str.decode('hex')
+	replace_str =  '{0:<0{1}x}'.format(int(translation_tuples_s[index][1].encode('hex'),16),len(find_str.decode('hex'))+2)
+	print "A ", find_str
+
+	print "B ", replace_str
+#	results = s.findall(find_str, bytealigned=True)
+#https://github.com/sans-dfir/sift-files/blob/master/scripts/pe_carve.py
+##	for i in results:
+#	    print("Found start code at byte offset %d." % (int(i)/8))
+#	    s0f0, length, bitdepth, height, width = s.readlist('hex:16, uint:16,   uint:8, 2*uint:16')
+#	    print("Width %d, Height %d" % (width, height))
+#https://stackoverflow.com/questions/29624398/searching-and-replacing-in-binary-file
+	s.replace("0x0"+find_str+"00","0x0"+replace_str+"00")
+	    
+	with open("fileToWriteTo", "wb") as output_file:
+	    output_file.write(s)
+	sys.exit(12)
 
 	#url =  "%".join(c for c in line.split())
 	#print "%".join("{:02x}".format(ord(c)) c for c in line)
