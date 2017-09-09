21fa956790cc043fcb3e760219f6d95b6b15edc0
dpeddi <eddi@dpeddi.com>
2017-09-09 22:44:06 +0200

    fix a bit the code.. now support just bmp and png. png with transparencies still convert back with issues

--- res2img.py
+++ res2img.py
@@ -32,7 +32,7 @@ parser.add_argument('-l', '--language', dest='language', default="en", #choices=
 parser.add_argument('-i', '--input', type=str, dest='input', help='input name of resource file (default:%(default)s)', default='Mili_chaohu.res')
 parser.add_argument('-a', '--auto_translate', action='store_true', dest='translate', help='auto translate resource file using known dict')
 
-parser.add_argument('-f', '--format', type=str, dest='imgfmt', help='format of img to generate', default='bmp')
+parser.add_argument('-f', '--format', type=str, dest='imgfmt', help='format of img to generate', choices=["png","bmp"], default='png')
 
 parser.add_argument('-u', dest='unpack', action='store_true', help='unrepack the res file')
 parser.add_argument('-p', dest='pack', action='store_true', help='repack the res file')
@@ -124,7 +124,10 @@ def gen_raw(idx):
 	#print "QUI1",img,os.path.getmtime(img)
 
 	if os.path.getmtime(raw) != os.path.getmtime(img):
-		cmd = "identify -format %%[depth] %s%s%03d.%s" % (dirName, os.path.sep, idx, imgfmt)
+		if args.imgfmt == "bmp":
+		    cmd = "identify -format %%[bit-depth] %s%s%03d.%s" % (dirName, os.path.sep, idx, imgfmt)
+		else:
+		    cmd = "identify -format %%[png:IHDR.bit_depth] %s%s%03d.%s" % (dirName, os.path.sep, idx, imgfmt)
 		p = subprocess.Popen(cmd.split(" "), stdout=subprocess.PIPE,
 						   stderr=subprocess.PIPE)
 		depth, err = p.communicate()
@@ -155,13 +158,13 @@ def gen_raw(idx):
 			    if m.groups()[3] == "0":
 				transparency=1
 			except Exception as e:
-			    print e
+			    #print e
+			    pass
 		#print "idx =" + str(idx)+" PALETTE=",palette
 
-		if transparency == 1:
-		    stride=int(int(width) * int(depth) / 64) + ((int(width) * int(depth) )% 64 > 0)
-		else:
-		    stride=int(int(width) * int(depth) / 8) + ((int(width) * int(depth) )% 8 > 0)
+		#if transparency == 1:
+		#    depth=str(int(depth)/8)
+		stride=int(int(width) * int(depth) / 8) + ((int(width) * int(depth) )% 8 > 0)
 
 		header_bmp = [ 0x42, 0x4D, 0x64, 0x00, int(width), 0x00, int(height) , 0x00, stride, 0x00, int(depth) , 0x00, len(palette) /4, 0 ,transparency ,0 ]
 		#print ["%2x" % c for c in header_bmp]
@@ -262,12 +265,12 @@ def get_bmp(idx):
 
 		#tmpA = mapcolor.mpc
 		cmd = "convert -quiet -size %dx%d+%d -depth %d +antialias -compress NONE gray:%s.raw +repage %s.mpc" % (width,height,16 + palette_len * 4,depth,filename, filename)
-		#print cmd
+		#print "DEBUG:", cmd
 		os.system(cmd)
 		#ww=`convert $tmpA -ping -format "%w" info:`
 		#hh=`convert $tmpA -ping -format "%h" info:`
 		cmd = "convert -size %dx%d xc:none %s.miff" % (width,height,filename)
-		#print cmd
+		#print "DEBUG:", cmd
 		os.system(cmd)
 
 		#cmd = "convert -verbose -channel rgba -alpha on "
@@ -286,7 +289,7 @@ def get_bmp(idx):
 			#-fill "$color2" -opaque "$color1" \) $tmp0 \
 			#-composite $tmp0
 		    except Exception as e:
-			print(e)
+			#print e
 			pass
 
 		#cmd += " -size "+str(width)+"x"+str(height)+"+"+str(16 + palette_len * 4) +" -depth " + str(depth) + " +antialias -compress NONE gray:"+filename+".raw "
@@ -331,7 +334,7 @@ with open(fileName, mode='rb') as file: # b is important -> binary
 	    print "file isn't a resource file. Exiting"
 	    os.exit(1)
 	print "file is a Haumi resource file"
-	print "version??? %d" % version
+	print "version %d" % version
 
 	buf = [ ord(elem) for elem in fileContent[0x10:0x10+4]]
 	max_rsrc = (buf[0] <<0) + (buf[1] <<8) + (buf[2] << 16) + (buf[3] <<24)
@@ -350,6 +353,7 @@ if args.unpack:
 	    #extract all bitmap and translate the one in the list
 	    extract_list = range(max_rsrc)
 
+	#extract_list = [ 66, 67, 181, 200, 204, 205]
 	for index in extract_list:
 
 	    addr = get_rsrc_addr(index)
@@ -362,7 +366,8 @@ if args.unpack:
 	    get_bmp(index)
 
 	#just for me... uncomment to extract just an image
-	#get_bmp(66)
+	#get_bmp(127)
+	#get_bmp(181)
 
 #pack
 if args.pack:
